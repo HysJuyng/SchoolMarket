dec18cf09c8778c1ddbce593a7dd9cf61799a513
Linjy <773780366@qq.com>
2016-03-31 14:15:17 +0800

    Initial Commit

---
+++ SchoolMarket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolMarket
+//
+//  Created by linjy on 16/3/31.
+//  Copyright © 2016年 linjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SchoolMarket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolMarket
+//
+//  Created by linjy on 16/3/31.
+//  Copyright © 2016年 linjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
