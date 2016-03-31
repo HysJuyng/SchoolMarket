b48edfb546cdce6a016a26d57732cff7d68dde8d
Linjy <773780366@qq.com>
2016-03-31 14:28:54 +0800

    init2

---
+++ SchoolMarket/Controller/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  SchoolMarket
+//
+//  Created by linjy on 16/3/31.
+//  Copyright © 2016年 linjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ SchoolMarket/Controller/RootTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  RootTabBarController.h
+//  SchoolMarket
+//
+//  Created by linjy on 16/3/31.
+//  Copyright © 2016年 linjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootTabBarController : UITabBarController
+
+@end
